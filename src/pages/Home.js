8cbc0d26264c963fdef4317d7342711979cd984b
ECHOSHIH <chunyushih@outlook.com>
2022-04-06 02:14:46 +0800

    [doc] route pages

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/users/UserSearch'
+const Home = () => {
+  return (
+    <>
+      {/* SEARCH COMPONENTS */}
+      <UserSearch />
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/NotFound.js
@@ -0,0 +1,22 @@
+import { FaHome } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+const NotFound = () => {
+  return (
+    <div className='hero'>
+      <div className='hero-content text-center'>
+        <div className='max-w-lg'>
+          <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
+          <p className='text-5xl mb-8'>404 - Page not found</p>
+          <Link to='/'>
+            <div className='btn btn-primary btn-lg'>
+              <FaHome className='mr-2' />
+              Back To Home
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
