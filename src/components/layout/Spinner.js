50165e6727c242b744546408eb12eb8602bf0476
ECHOSHIH <chunyushih@outlook.com>
2022-04-06 02:12:11 +0800

    [doc] layout

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+const Spinner = () => {
+  return (
+    <>
+      <div className='text-center w-100'>
+        <div class='btn btn-ghost btn-lg loading'></div>
+      </div>
+    </>
+  )
+}
+
+export default Spinner
