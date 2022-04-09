948ee23ff78d4ac5f5d7210f63418787afeddd42
ECHOSHIH <chunyushih@outlook.com>
2022-04-09 15:32:59 +0800

    [ft] get user repos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from 'react'
+import { createRenderer } from 'react-dom/test-utils'
 import { useNavigate } from 'react-router-dom'
 import githubReducer from './GithubReducer'
 
@@ -12,6 +13,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   }
   const [state, dispatch] = useReducer(githubReducer, initialState)
@@ -35,29 +37,50 @@ export const GithubProvider = ({ children }) => {
       })
     }, 1000)
   }
+
   // get single user result
   const getUser = async (login) => {
     //[loading時間設定]啟動fetchUser會先設定loading => true
     setLoading()
-
     const response = await fetch(`${GITHUB_URL}/users/${login}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
-    if ((response.status = 404)) {
+    if (response.status === 404) {
       window.location = '/notfound'
     } else {
       const data = await response.json()
-      //[loading時間設定]啟動fetchUser=> loading === true => 1.3秒後 loading ===false
-      setTimeout(() => {
-        dispatch({
-          type: 'GET_USER',
-          payload: data,
-        })
-      }, 1000)
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
     }
   }
+
+  // get user repos
+  const getUserRepos = async (login) => {
+    //[loading時間設定]啟動fetchUser會先設定loading => true
+    setLoading()
+    const params = new URLSearchParams({ sort: 'created_at', per_page: 10 })
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    )
+    const data = await response.json()
+    //[loading時間設定]啟動fetchUser=> loading === true => 1.3秒後 loading ===false
+    setTimeout(() => {
+      dispatch({
+        type: 'GET_REPOS',
+        payload: data,
+      })
+    }, 1000)
+  }
+
   const setLoading = () => {
     dispatch({ type: 'SET_LOADING' })
   }
@@ -70,9 +93,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         getUser,
         clearUsers,
+        getUserRepos,
       }}
     >
       {children}
