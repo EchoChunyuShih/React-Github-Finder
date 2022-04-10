ce9569bf63546bcd4ded91fcd6d1fef13731f0e3
ECHOSHIH <chunyushih@outlook.com>
2022-04-10 16:35:02 +0800

    [refactor] create common headers and merge getUser and getRepo into one

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,42 +1,27 @@
+import axios from 'axios'
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
+})
 
 // get search result
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({ q: text })
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
-  const { items } = await response.json()
-  //[loading時間設定]啟動fetchUser=> loading === true => 1.3秒後 loading ===false
-  return items
+  const response = await github.get(`/search/users?${params}`)
+  return response.data.items
 }
 
-// get single user result
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
-  if (response.status === 404) {
-    window.location = '/notfound'
-  } else {
-    const data = await response.json()
-    return data
-  }
-}
-
-// get user repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({ sort: 'created_at', per_page: 10 })
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
-  const data = await response.json()
-  return data
+// get single user and repos
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`users/${login}/repos`),
+  ])
+  return { user: user.data, repos: repos.data }
 }
