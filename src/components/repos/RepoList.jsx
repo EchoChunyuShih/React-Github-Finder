03eca4741fee55d089434046a1d171161de966eb
Echo Shih <chunyushih@outlook.com>
2022-05-13 03:13:37 +0800

    [docs] update UI - use grid

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -6,13 +6,15 @@ const RepoList = ({ repos }) => {
   return (
     <>
       <div className='rounded-lg shadow-lg card bg-base-100'>
-        <div className='card-body'>
+        <div className='card-body w-full'>
           <h2 className='text-3xl font-bold my-4 card-title'>
             Latest Repositories
           </h2>
-          {repos.map((repo) => (
-            <RepoItem key={repo.id} repo={repo} />
-          ))}
+          <div className='grid grid-cols-3 items-center gap-3'>
+            {repos.map((repo) => (
+              <RepoItem key={repo.id} repo={repo} />
+            ))}
+          </div>
         </div>
       </div>
     </>
