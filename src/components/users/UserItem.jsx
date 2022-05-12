4e503d5f2654f8e2236f206bb3c78d067612c030
Echo Shih <chunyushih@outlook.com>
2022-05-13 03:12:28 +0800

    [docs] update UI - change layout and colors - add transition animation

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -2,7 +2,10 @@ import { Link } from 'react-router-dom'
 const UserItem = ({ user: { login, avatar_url } }) => {
   return (
     <>
-      <div className='card shadow-md compact side bg-base-100 my-1 mx-1'>
+      <Link
+        className='card text-slate-50 text-opacity-70 hover:bg-slate-300 hover:shadow-lg hover:text-gray-800 mx-1 hover:cursor-pointer shadow-md card-compact bg-slate-700 my-2 transition duration-400 ease-out hover:ease-in'
+        to={`/user/${login}`}
+      >
         <div className='flex-row items-center space-x-4 card-body'>
           <div className='avatar'>
             <div className='rounded-full shadow w-14 h-14'>
@@ -10,16 +13,13 @@ const UserItem = ({ user: { login, avatar_url } }) => {
             </div>
           </div>
           <div>
-            <h2 className='card-title'>{login}</h2>
-            <Link
-              className='text-base-content text-opacity-40'
-              to={`/user/${login}`}
-            >
-              visit profile
-            </Link>
+            <h2 className='card-title tracking-wide mono'>
+              <span>@</span>
+              {login}
+            </h2>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   )
 }
