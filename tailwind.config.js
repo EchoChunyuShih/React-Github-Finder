6527f401233715d71cc6594677f319072cf5a098
Echo Shih <chunyushih@outlook.com>
2022-05-13 03:19:41 +0800

    [docs] customized theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,43 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-in': {
+          '0%': {
+            '-webkit-transform': 'translateX(-200px)',
+            transform: 'translateX(-200px)',
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0px)',
+            transform: 'translateX(0px)',
+          },
+        },
+
+        'slide-fwd': {
+          '0%': {
+            '-webkit-transform': 'translateZ(0px)',
+            transform: 'translateZ(0px)',
+          },
+          '100%': {
+            '-webkit-transform': 'translateZ(160px)',
+            transform: 'translateZ(160px)',
+          },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 1s ease-out',
+        'slide-fwd':
+          'slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+      },
+      gridTemplateColumns: {
+        //for repo list
+        3: 'repeat(auto-fit, minmax(300px, 1fr))',
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
-    theme: ["cyberpunk", "garden"],
+    theme: ['cyberpunk', 'garden'],
   },
-};
+}
