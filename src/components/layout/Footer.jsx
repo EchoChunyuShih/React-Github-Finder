3732983a1aa223186ccf1a8bf1a6a34084972be9
Echo Shih <chunyushih@outlook.com>
2022-05-15 13:22:08 +0800

    [docs] update

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,24 +4,23 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   const footerYear = new Date().getFullYear()
   return (
-    <footer className='footer pt-10 pb-5 bg-base-200 text-primary-content footer-center'>
-      <BsCodeSlash size={30} />
-      <div className='flex justify-center items-center text-slate-400 w-full '>
+    <footer className='footer pt-10 pb-5 footer-center'>
+      <div className='flex justify-center items-center text-slate-gray w-full'>
         <div> &copy; {footerYear} </div>
-        <div className='ml-1 text-slate-400 flex'>
-          Built with
-          <Link to='https://tailwindcss.com/' className='text-sky-400 ml-1'>
+        <div className='ml-1 flex'>
+          Built with&nbsp;
+          <Link to='https://tailwindcss.com/' className='text-neon-green ml-1'>
             React v17
           </Link>
           ,
-          <Link to='https://tailwindcss.com/' className='text-sky-400 ml-1'>
+          <Link to='https://tailwindcss.com/' className='text-neon-green ml-1'>
             Tailwind
           </Link>
           ,
-          <Link to='https://daisyui.com/' className='text-sky-400 mx-1'>
+          <Link to='https://daisyui.com/' className='text-neon-green mx-1'>
             Daisy UI
           </Link>
-          and more...
+          &nbsp;and more...
         </div>
       </div>
     </footer>
