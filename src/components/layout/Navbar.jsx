22a6dd699b656fd34f3d26c2857ab0f3c6b4f33a
Echo Shih <chunyushih@outlook.com>
2022-05-16 23:07:22 +0800

    [style] udate UI

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,14 +4,12 @@ import PropTypes from 'prop-types'
 
 const Navbar = ({ title }) => {
   return (
-    <nav className='navbar mb-1 text-neon-green pt-5'>
-      <div className='container'>
-        <div className='flex-none'>
-          <FaGithub className='inline pr-2 text-3xl' />
-          <Link to='/' className='text-lg font-bold align-middle'>
-            {title}
-          </Link>
-        </div>
+    <nav className='navbar mb-1 text-neon-green pt-5 mx-5'>
+      <div className='flex-none'>
+        <FaGithub className='inline pr-2 text-3xl' />
+        <Link to='/' className='text-lg font-bold align-middle'>
+          {title}
+        </Link>
       </div>
     </nav>
   )
