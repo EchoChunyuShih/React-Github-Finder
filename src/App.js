3a84f7d210a43da856a48979c9583dfe8f98119f
Echo Shih <chunyushih@outlook.com>
2022-05-16 23:43:16 +0800

    [style]

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Footer from './components/layout/Footer'
 import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import User from './pages/User'
-import About from './pages/About'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './context/github/GithubContext'
 import { AlertProvider } from './context/alert/AlertContext'
@@ -22,7 +21,7 @@ function App() {
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/user/:login' element={<User />} />
-                {/* <Route path='/about' element={<About />} /> */}
+
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='/*' element={<NotFound />} />
               </Routes>
