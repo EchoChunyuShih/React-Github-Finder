a48ab0947b683fb47d569dc410dfbd7abb30b3a7
Echo Shih <chunyushih@outlook.com>
2022-05-17 09:31:34 +0800

    [docs] update UI - add more language colors

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -19,26 +19,32 @@ const RepoItem = ({ repo }) => {
 
   const lang_color = (lang) => {
     const lang_colors = {
-      JavaScript: 'bg-amber-300',
-      PHP: 'bg-sky-700',
-      HTML: 'bg-orange-600',
-      CSS: 'bg-indigo-700',
-      TypeScript: 'bg-cyan-700',
-      Python: 'bg-blue-500',
-      Vue: 'bg-emerald-700',
+      C: 'bg-red-400',
       'C++': 'bg-pink-500',
       'C#': 'bg-pink-700',
+      CSS: 'bg-indigo-700',
+      CoffeeScript: 'bg-amber-800',
+      Clojure: 'bg-sky-500',
+      Go: 'bg-sky-300',
+      HTML: 'bg-orange-600',
       Haxe: 'bg-orange-400',
+      Java: 'bg-yellow-800',
+      JavaScript: 'bg-amber-300',
+      'Jupyter Notebook': 'bg-fuchsia-300',
+      Kotlin: 'bg-amber-400',
+      Lua: 'bg-rose-200',
+      'Objective-C': 'bg-green-800',
+      PHP: 'bg-sky-700',
+      Python: 'bg-blue-500',
+      Perl: 'bg-fushia-600',
+      Ruby: 'bg-rose-500',
+      Matlab: 'bg-lime-500',
+      TypeScript: 'bg-cyan-700',
+      TeX: 'bg-lime-200',
       Scala: 'bg-red-700',
       Shell: 'bg-green-300',
-      Java: 'bg-yellow-800',
-      Perl: 'bg-fushia-600',
-      Lua: 'bg-rose-200',
-      'Jupyter Notebook': 'bg-fushia-300',
-      Matlab: 'bg-lime-500',
-      C: 'bg-red-400',
-      TeX: 'bg-lime-200',
-      Kotlin: 'bg-amber-400',
+      Swift: 'bg-orange-500',
+      Vue: 'bg-emerald-700',
     }
     return lang_colors[lang]
   }
@@ -47,7 +53,7 @@ const RepoItem = ({ repo }) => {
     <div className='repo-item mb-2 shadow-md rounded-md bg-slate-300 px-4 py-4 h-full flex flex-col justify-between hover:cursor-pointer hover:bg-dark transition duration-400 ease-out hover:ease-in'>
       <a
         href={html_url}
-        rel='noreferer'
+        rel='noreferrer'
         target='_blank'
         className='repo-title flex items-center justify-start text-dark text-xl'
       >
